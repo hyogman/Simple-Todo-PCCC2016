69fc453bb5414ba4d1389d3090767f9e88288fd2
henry <henryyogman@gmail.com>
2016-08-08 16:50:23 -0600

    added most functionality and some styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,24 @@
+// Check off specific Todos by clicking
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
 
+// click on x to delete todo
+$("ul").on("click", "span", function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event) {
+  if(event.which === 13) {
+    // grab new todo text from input
+    var todoText = $(this).val();
+    // clear input
+    $(this).val("");
+    // create a new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+
+  }
+});
